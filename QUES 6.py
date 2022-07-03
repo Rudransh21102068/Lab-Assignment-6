29e4ec1313f51213ea0e87581edd634cb2cbd6c9
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-07-03 23:24:10 +0530

    Update QUES 6.py

--- QUES 6.py
+++ QUES 6.py
@@ -1,14 +1,13 @@
-order=input("What do you want to print SID,name or class:") 
-def student_data(student_id,**data):
-    if order=="sid":
-            print("\nStudent ID:",student_id)
-    elif order=="name" or order=="class":
-            print("Student name:",{data['student_name']}, "Student class:",{data['student_class']})
-            print("\n")
+def student_data(student_id, **kwargs):
+    print(f'\nStudent ID: {student_id}')
+    if 'student_name' in kwargs:
+        print(f"Student Name: $ {kwargs['student_name']}")
     
+    if 'student_name' and 'student_class' in kwargs:
+            print(f"\nStudent Name: $ {kwargs['student_name']}")
+            print(f"Student Class: $ {kwargs['student_class']}")
 
+ 
+student_data(student_id='21102068', student_name='Rudransh')
 
-
-student_data(21102068, student_name="Rudransh", student_class="B.tech")
-student_data(21102069, student_name="Dev Bohat",student_class="B.tech")
-student_data(21102074, student_name="Lapata", student_class="B.tech")
+student_data(student_id='21102068', student_name='Rudransh', student_class ='B.Tech')
